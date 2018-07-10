796666c0c602e90b5186b8ab95767779b95a3568
Alex Moser <alex@shopfollain.com>
2018-07-10 14:15:30 -0400

    basic error handling setup

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -7,15 +7,32 @@ export const resolvers: ResolverMap = {
     bye: () => "bye"
   },
   Mutation: {
-    register: async (_, { email, password }: GQL.IRegisterOnMutationArguments) => {
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const userAlreadyExists = await User.findOne({
+        where: { email },
+        select: ["id"]
+      });
+
+      if (userAlreadyExists) {
+        return [
+          {
+            path: "email",
+            message: "already taken"
+          }
+        ];
+      }
+
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = User.create({
         email,
-        password: hashedPassword,
-      })
+        password: hashedPassword
+      });
 
       await user.save();
-      return true;
+      return null;
     }
   }
 };
