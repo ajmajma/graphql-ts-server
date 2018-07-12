396cb6d9347bb2b620e3abacfd20c9e57a17668f
Alex Moser <alex@shopfollain.com>
2018-07-12 10:33:59 -0400

    refactor tests and code, sets up GQL type generation

--- src/testSetup/callSetup.js
+++ src/testSetup/callSetup.js
@@ -5,6 +5,10 @@ const { setup } = require("./setup");
 
 module.exports = async function() {
   // Call your initialization methods here.
-  await setup();
+
+  // if server not init
+  if (!process.env.TEST_HOST) {
+    await setup();
+  }
   return null;
 };
